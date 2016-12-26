ce9175fdb7793bd264a52c9e58be9d82aedc5b52
uponstars <1746572317@qq.com>
2016-12-26 11:49:27 +0800

    modify

---
+++ Controller/Comment/forum.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace \Controller\Comment;
+
+use \Model\Tables\Forum;
+use \PDO;
+
+//引入要实例化的类
+include '../Model/Forum.class.php ';
+header('Content-Type:text/html;charset=utf-8');
+$floor            = 0;    //需要缩进的数
+$contentStr       = '';
+$forum            = new Forum(false);     //实例化
+$selectArgs = array(        //设置属性
+    'role' => 'replySelectDESC',
+    'data' => array(
+        'fid' => 0,
+    ),
+    "level" => 0
+);
+try {
+    $forum->base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $forum->base->beginTransaction();      //开启事务
+    $floor = sizeof($forum->getData($selectArgs));
+    getContent($forum, $selectArgs);           //罗列事务
+}
+catch (Exception $e) {
+    $forum->base->rollBack();                 //回滚
+    die('Error!: ' . $e->getMessage() . '<br />');
+}
+
+function getContent($Forum, $SelectArgs)
+{
+    $SelectArgs['level']++;
+    $_result = $Forum->getData($SelectArgs);      //查询事务
+    if ($_result) {            //查询到值
+        foreach ($_result as $_row) {       //选取每条数据
+            $_fid = $_row['fid'];
+            $SelectArgs['data']['fid'] = $_row['id'];        //设置递归属性
+            $SelectArgs['role'] = "replySelectASC";
+            if (!$_fid) {
+                $GLOBALS['contentStr'] .= "
+                <div class='row' data-goal='comment'>
+                <div class='col-md-2'><a><img src='#' /></a></div>
+                <div class='col-md-8'>
+                <div class='row'>
+                <a>" . $_row["uid"] . "</a>
+                </div>
+                <div class='row'>
+                <p>".$_row['content']."</p>
+                </div>
+                <div class='row'>
+                <p class='inner'>#". $GLOBALS['floor']-- . "</p>
+                <p class='inner'>" . $_row["sendtime"] . "</p>
+                <button class='zan btn btn-default inner'>赞（<span>" . $_row['agree'] . "</span>）</button>
+                <button type='button' class='btn btn-default dropdown-toggle inner review article' data-toggle='dropdown' data-open='1'>回复</button>
+                <button class='jubao btn btn-default inner'>举报</button>
+                </div>
+                ";
+            }
+            else {
+                $_uid = ($SelectArgs['level'] > 2) ? "@" . $SelectArgs['uid'] : "";
+                $GLOBALS['contentStr'] .= "
+                            <div class='row' data-goal='comment'>
+                            <div class='col-md-2'><a><img src='#' /></a></div>
+                            <div class='col-md-10'>
+                            <div class='row'>
+                            <a>" . $_row["uid"] . "</a><span>:</span>
+                            <p class='inner'><a href='#'>" . $_uid . "</a></p>
+                            <p class='inner'>".$_row['content']."</p>
+                            </div>
+                            <div class='row'>
+                            <p class='inner'>" . $_row["sendtime"] . "</p>
+                            <button class='zan btn btn-default inner'>赞（<span>" . $_row['agree'] . "</span>）</button>
+                            <button type='button' class='btn btn-default dropdown-toggle inner review discuss' data-toggle='dropdown' data-open='1'>回复</button>
+                            <button class='jubao btn btn-default inner'>举报</button>
+                            </div>
+                        </div>
+                    </div>
+                    ";
+            }
+            $SelectArgs['uid'] = $_row['uid'];
+            getContent($Forum, $SelectArgs);       //递归开始
+            if ($_fid == 0) {
+                $GLOBALS['contentStr'] .= "</div><div class='col-md-4'></div></div><hr/>";
+            }
+        }
+    }
+}
+  ?>
---
+++ View/Computer/header.php
@@ -0,0 +1,46 @@
+<!-- start header -->
+<header class="container-fluid bd" style="background-image: url(./images/)">
+    <!-- start nav -->
+    <nav class="navbar container-control">
+        <div class="container relative">
+            <ul class="nav nav-tabs" role="tablist">
+                <li role="presentation" class="active"><a href="#">NEWS</a></li>
+                <li role="presentation"><a href="#">COMICS</a></li>
+                <li role="presentation"><a href="#">IT SKILLS</a></li>
+                <li role="presentation"><a href="#">GUEST BOOKS</a></li>
+                <li role="presentation"><a href="#">GAMES</a></li>
+                <li role="presentation"><a href="#">PARTYS</a></li>
+            </ul>
+            <ul class="nav nav-pills keep-right">
+                <li><a href="">登陆</a></li>
+                <li><a href="#">/</a></li>
+                <li><a href="">注册</a></li>
+            </ul>
+        </div>
+    </nav>
+    <!-- end nav -->
+    <div class="container">
+        <div class="row">
+            <!-- start logo -->
+            <figure class="col-md-3" style="margin-top:15px;">
+                <a href="">
+                    <img src="./images/logo.svg" alt="" class="img-responsive" />
+                </a>
+            </figure>
+            <!-- end logo -->
+            <!-- start search -->
+            <form class=" form col-md-3 col-md-push-6" action="index.html" method="post" style="margin-top:50px;margin-bottom:20px;">
+                <div class="input-group">
+                    <input type="text" class="form-control">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="button">
+                            <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                    </span>
+                </div>
+            </form>
+            <!-- end search -->
+        </div>
+    </div>
+</header>
+<!-- end header -->
